292ce7998b44e45ab33c00b670458527e40a080c
Сергей Карбовский <kserx05@rambler.ru>
2015-11-21 16:44:15 +0400

    Noise adds to square signal too

--- a7d1_gen.m
+++ a7d1_gen.m
@@ -3,17 +3,19 @@ function f = a7d1_gen(~)
     frequency = 50;
     samplesNumber = 500;
     dt = 0.001;
-    noiseAmplitude = 0.5;
+    noiseAmplitude = 0.3;
     
     x = dt * (0: samplesNumber-1);
     f = zeros(CHANNELS, samplesNumber);
     digNumber = randi(CHANNELS);
     for i = 1:CHANNELS
+        noise = noiseAmplitude * (rand(size(x)) - 0.5);
         if i == digNumber
-            f(i, :) = (square(2*pi*frequency*x) + 1) / 2;
+            signal = (square(2*pi*frequency*x) + 1) / 2 + noise;
         else
-            f(i, :) = sin(2*pi*frequency*x + pi*rand) + noiseAmplitude * (rand(size(x)) - 0.5);
+            signal = sin(2*pi*frequency*x + pi*rand) + noise;
         end
+        f(i, :) = signal + noise;
         subplot(CHANNELS, 1, i);
         plot(x, f(i, :))
     end
